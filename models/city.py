57ca5719eb8bffcbd8e0d3f97d6a1e111d8f4430
Theophilus Samuel <36473098+ConnectedDot@users.noreply.github.com>
2023-09-23 09:40:39 +0100

    updated city to use DBStorage

--- models/city.py
+++ models/city.py
@@ -3,7 +3,7 @@
 from models.base_model import BaseModel
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """Represent a city.
 
     Attributes:
@@ -11,13 +11,25 @@ class City(BaseModel):
         name (str): The name of the city.
     """
 
-    def __init__(self, *args, **kwargs):
-        """
-            Initialize clss user with kwargs
-            Args:
-                *args(positional arg): strings
-                **kwargs(keyword arg): dictionary
-        """
-        super().__init__(*args, **kwargs)
-        self.state_id = ""
-        self.name = ""
+    from sqlalchemy import Column, String, ForeignKey
+    from models.base_model import BaseModel
+    from sqlalchemy.orm import relationship
+
+    class City(BaseModel):
+        """Represent a city."""
+
+        __tablename__ = "cities"
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+        # places = relationship("Place", backref="cities")
+
+        def __init__(self, *args, **kwargs):
+            """
+                Initialize clss user with kwargs
+                Args:
+                    *args(positional arg): strings
+                    **kwargs(keyword arg): dictionary
+            """
+            super().__init__(*args, **kwargs)
+            self.state_id = ""
+            self.name = ""

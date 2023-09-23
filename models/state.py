e7282d7044357831e4093c90bf576f82be4e1c03
Theophilus Samuel <36473098+ConnectedDot@users.noreply.github.com>
2023-09-23 09:41:00 +0100

    updated state to use DBStorage

--- models/state.py
+++ models/state.py
@@ -1,21 +1,35 @@
 #!/usr/bin/python3
 """Defines the State class."""
+import os
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel
+from models.city import City
+from models import storage
 
 
 class State(BaseModel):
-    """Represent a state.
+    """Represent a state."""
 
-    Attributes:
-        name (str): The name of the state.
-    """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    # For DBStorage
+    if os.environ.get("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", backref="state",
+                              cascade="all, delete-orphan")
+    # For FileStorage
+    else:
+        @property
+        def cities(self):
+            return [city for city in list(storage.all(City).values())
+                    if city.state_id == self.id]
 
     def __init__(self, *args, **kwargs):
         """
-            Initialize clss user with kwargs
-            Args:
-                *args(positional arg): strings
-                **kwargs(keyword arg): dictionary
+        Initialize class user with kwargs
+        Args:
+            *args(positional arg): strings
+            **kwargs(keyword arg): dictionary
         """
         super().__init__(*args, **kwargs)
         self.name = ""

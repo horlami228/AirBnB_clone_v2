88e955056d45210cf768537961d8d8461e0f50e1
Olamilekan <akintolaolamilekan51@gmail.com>
2023-10-07 18:56:41 +0100

    deploy

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,14 +1,36 @@
 #!/usr/bin/python3
 
 """
-    deploy web static
+    This a fabric module for deploying to my web server
+    in a automated way
 """
-from fabric.api import run, put, env, local
+
+from fabric.api import local, run, put, env
+from time import strftime
 from os import path
+
 env.hosts = [
         "ubuntu@54.174.245.243",
         "ubuntu@3.84.158.22"
 ]
+
+
+def do_pack():
+
+    """
+        This function converts and compress the contents
+        of web_static folder in an archive
+    """
+
+    time = strftime("%Y%m%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        local("tar -cvzf versions/web_static_{}.tgz web_static/"
+              .format(time))
+
+        return "versions/web_static_{}.tgz".format(time)
+    except Exception:
+        return None
 
 
 def do_deploy(archive_path):
@@ -56,3 +78,14 @@ def do_deploy(archive_path):
     except Exception as e:
         return False
     return True
+
+
+def deploy():
+    """
+        pack and deploy to web server
+    """
+    path_to_archive_file = do_pack()
+    if path_to_archive_file is None:
+        return False
+    status = do_deploy(archive_path=path_to_archive_file)
+    return status

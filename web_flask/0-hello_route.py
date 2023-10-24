2dc480bcfab843760765238d443f9bcde6d71596
Olamilekan <akintolaolamilekan51@gmail.com>
2023-10-24 12:58:03 +0100

    flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-from flask import Flask
-
 """
     This is a script that starts a Flask
     web application
 """
+from flask import Flask
+
 app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def hello():

fbb7af7f219bebbf05c2154f1884d488329799bc
Olamilekan <akintolaolamilekan51@gmail.com>
2023-10-24 12:55:22 +0100

    flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from flask import Flask
+
+"""
+    This is a script that starts a Flask
+    web application
+"""
+app = Flask(__name__)
+@app.route("/", strict_slashes=False)
+def hello():
+    """ Home page"""
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+    
